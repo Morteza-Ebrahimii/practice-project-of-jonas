6d020084239238f70b0d51272f19ab6fc88af1c3
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-11-24 12:52:56 +0330

    feat: make friends component

---
+++ eatSplit/eatSplit/src/data/friends.js
@@ -0,0 +1,7 @@
+import img from './img/test.webp'; 
+
+export const friends = [
+    { id: 4243, name: 'sarah', image:'https://i.pravatar.cc/48?u=118836', balance: 24 },
+    { id: 356, name: 'jenifer', image: 'https://i.pravatar.cc/48?u=933372', balance: -45 },
+    { id: 429843, name: 'jack', image: 'https://i.pravatar.cc/48?u=499476', balance: 0 },
+];
